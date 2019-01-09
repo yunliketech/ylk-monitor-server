24002d675606425e8468a31c156d175438922a5d
danny00lo <860222083@qq.com>
2019-01-09 18:01:43 +0800

    增加分表上传

--- app/service/logs.js
+++ app/service/logs.js
@@ -5,15 +5,46 @@ const Service = require('egg').Service;
 class LogsService extends Service {
   async addData (data) {
     const list = [], listError = [], listDiy = [], listPerformance = [];
+    let error = false, diy = false, performance = false;
 
     console.log(list)
     for (let i = 0; i < data.log.length; i++) {
-      list.push([data.time, data.device, JSON.stringify(data.log[i]), data.log[i].type, 1]);
+      // 传入分表diy
+      if (data.log[i].type === 'DIY') {
+        diy = true;
+        listDiy.push([data.time, data.device, data.log[i].type, 1, data.log[i].msg]);
+      }
+      // 传入分表error
+      if (data.log[i].type === 'error') {
+        error = true;
+        listError.push([data.time, data.device, data.log[i].type, 1,]);
+      }
+      // 传入分表performance
+      if (data.log[i].type === 'performance') {
+        performance = true;
+        listPerformance.push([data.time, data.device, data.log[i].type, 1,]);
+      }
+      // 传入总表
+      list.push([data.time, data.device, data.log[i].type, 1, JSON.stringify(data.log[i])]);
     }
     console.log("查询", list)
     // 总表输入
-    const result = await this.app.mysql.query("INSERT INTO c_logs (time, device, log, type, project_id) VALUES ?", [list])
-    // 分表插入
+    const result = await this.app.mysql.query("INSERT INTO c_logs (time, device, type, project_id, log) VALUES ?", [list])
+    // 分表插入 -没有则跳过
+    if (diy) {
+      const diyResult = await this.app.mysql.query("INSERT INTO c_logs_diy (time, device, type, project_id, msg) VALUES ?",
+        [listDiy])
+    }
+    if (error) {
+      const errorResult = await this.app.mysql.query("INSERT INTO c_logs_error (time, device, type, project_id) VALUES ?",
+        [listError]);
+    }
+    if (performance) {
+      const performanceResult = await this.app.mysql.query("INSERT INTO c_logs_performance" +
+        "(time, device, type, project_id, navigationStart, unloadEventStart, unloadEventEnd, redirectStart, redirectEnd, fetchStart," +
+        +"domainLookupStart, domainLookupEnd) VALUES ?",
+        [listPerformance])
+    }
 
     return { result };
   }
