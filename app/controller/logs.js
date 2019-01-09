6622a3942790afc007241f4600849d05ec11a23b
danny00lo <860222083@qq.com>
2019-01-09 09:32:31 +0800

    编写查询接口

--- app/controller/logs.js
+++ app/controller/logs.js
@@ -3,18 +3,21 @@
 const Controller = require('egg').Controller;
 
 class LogsController extends Controller {
-  async index () {
-    const { ctx, service } = this;
-    const result = await ctx.service.home.getData();
-    ctx.body = result;
-  }
-
   async add () {
     const { ctx, service } = this;
 
     const data = ctx.request.body;
     const result = await ctx.service.logs.addData(data);
     ctx.body = result
+  }
+
+  async list () {
+    const { ctx, service } = this;
+
+    const data = ctx.query;
+    const result = await ctx.service.logs.serachData(data);
+
+    ctx.body = result;
   }
 }
 
--- app/router.js
+++ app/router.js
@@ -7,5 +7,8 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
   // 展开数据保存
-  router.post('/logs/add', controller.logs.add);
+  router.post('/logs/add', controller.logs.add);  // 插入logs数据
+  router.get('/logs/list', controller.logs.list); // 查询logs数据
+
+  // router.post('/login')
 };
