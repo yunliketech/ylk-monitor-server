f977ddf5d0b11f093f3e2299dfbc9b3c8e030867
danny00lo <860222083@qq.com>
2019-01-11 10:06:52 +0800

    增加session校验验证

--- config/config.default.bak.js
+++ config/config.default.bak.js
@@ -3,11 +3,18 @@
 module.exports = appInfo => {
   const config = exports = {};
 
-  // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '';
-
   // add your config here
-  config.middleware = [];
+  config.middleware = ['errorHandler', 'auth'];
+
+  // 只对 /api 前缀的 url 路径生效
+  config.errorHandler = {
+    match: '/api',
+  };
+
+  // 只对 /api 前缀的 url 路径生效
+  config.auth = {
+    match: '/api',
+  };
 
   config.mysql = {
     client: {
@@ -19,6 +26,21 @@ module.exports = appInfo => {
     },
     app: true,      // 是否加载到app上
     agent: false    // 是否加载到agent上
+  };
+
+  //关闭csrf
+  config.security = {
+    csrf: {
+      enable: false,
+      ignoreJSON: true
+    },
+    // 白名单
+    domainWhiteList: ['http://127.0.0.1:8080']
+  }
+
+  config.cors = {
+    origin: '*',
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH'
   };
 
   return config;
